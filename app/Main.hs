cd516ffcbb245c44c60d0641b3505e002b123b70
Syllina <syllinxia@gmail.com>
2025-06-30 15:48:06 +0800

    feat: initial commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,84 @@
 module Main (main) where
 
-import Hsco (someFunc)
+import Hsco (chapterTitle)
+import Control.Lens
+import Control.Concurrent (threadDelay)
 
+import Network.Wreq as W hiding (manager)
+import Text.HTML.Scalpel hiding (manager)
+import Network.HTTP.Client (HttpException(HttpExceptionRequest))
+import Control.Exception (handle)
+
+import qualified Data.Text as T
+
+import TextShow
+
+import System.Directory (doesFileExist)
+
+import Text.Parsec
+
+repeatedGet delay _ _ | delay > 128000000 = pure Nothing
+repeatedGet delay opts url = handle (\(HttpExceptionRequest _ _) -> putTextLn ("Retrying " <> T.pack url <> "...") >> threadDelay delay >> repeatedGet (delay*2) opts url) $ Just <$> W.getWith opts url
+
+buildNewFile indexedList = do
+    writeFileText "ydhbjc.tex" "\\documentclass{ctexart}\n\\usepackage[margin=1in]{geometry}\n\\fontsize{18pt}{1.2}\n\\begin{document}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    writeFileText "tmp.in" ""
+    forM_ indexedList $ \(idx, (name, _)) -> whenM (doesFileExist $ "chap/ch" <> show idx <> ".tex") $ do
+        content <- readFileBS $ "chap/ch" <> show idx <> ".tex"
+        let toSection (Right (1, _)) = "\\section{unknown}\n\\subsection*{" <> name <> "}\n"
+            toSection _ = "\\subsection*{" <> name <> "}\n"
+            -- toSection _ = "what? " <> name <> "\n"
+        appendFileText "ydhbjc.tex" $ toSection $ parse chapterTitle "" name
+        appendFileBS "ydhbjc.tex" content
+
+
+    appendFileText "ydhbjc.tex" "\\end{document}"
 
 main :: IO ()
-main = someFunc
+main = do
+    -- let url = "http://www.ydhbjc.com/169_169734/64239730.html"
+    let root = "http://www.ydhbjc.com"
+    let bookID = "/169_169734"
+    let opts = defaults
+
+    r <- W.getWith opts $ root <> bookID
+    let content = decodeUtf8 $ r^.responseBody :: Text
+    writeFileText "toc.html" content
+    
+    -- putTextLn content
+    -- whenJust (scrapeStringLike content (text $ "div" @: ["id" @= "content"])) putTextLn
+    let Just chapterList = scrapeStringLike content getTOC where
+        getTOC = chroot ("div" @: ["id" @= "list"]) $ chroots "dd" parseSingle
+        parseSingle = do
+            chapterName <- text "a"
+            chapterURL <- attr "href" "a"
+            pure (chapterName, chapterURL)
+
+    putTextLn $ "Total chapters: " <> (showt $ length chapterList)
+    writeFileText "toc" $ T.unlines $ map (\(idx, (name, url)) -> showt idx <> ": " <> name) $ zip ([1..] :: [Int]) chapterList
+
+    let indexedList = zip ([0..] :: [Int]) chapterList
+
+    buildNewFile indexedList
+
+    let processCond (idx, _) = not <$> (doesFileExist $ "chap/ch" <> show idx <> ".tex")
+        processCond _ = pure True
+    -- processList <- filterM processCond (zip ([0..] :: [Int]) chapterList) where cond = pure False
+    -- why wrong?
+    processList <- filterM processCond indexedList
+            
+    forM_ processList $ \(idx, (name, url)) -> do
+        threadDelay 1000000
+        res <- repeatedGet 1000000 opts $ root <> T.unpack url
+        case res of
+            Just r -> do
+                let content = decodeUtf8 $ r^.responseBody :: Text
+
+                let Just chapterContent = scrapeStringLike content (text $ "div" @: ["id" @= "content"])
+
+                -- when (idx `mod` 10 == 0) $ threadDelay 5000000
+                putTextLn $ "Processing " <> showt idx <> "/1043"
+
+                let result = (T.replace (T.singleton '\r') "\n\n" chapterContent) <> "\n" :: Text
+                writeFileText ("chap/ch" <> show idx <> ".tex") result
+            Nothing -> pure ()
--- src/Hsco.hs
+++ src/Hsco.hs
@@ -8,8 +8,43 @@ The very least comprehensive personal haskell tool
 
 module Hsco
        ( someFunc
+       , chapterTitle
        ) where
 
+import Prelude hiding ((<|>), many)
+
+import Text.Parsec
+import Text.Parsec.Text
+
+import qualified Data.Text as T
+
+import Data.Char (isPrint)
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
+singleNum :: Parser Int
+singleNum = hz2Int <$> oneOf orderedZhNums where
+    orderedZhNums = "零一二三四五六七八九"
+    hz2Int ch = fromMaybe (-1) $ T.findIndex (ch ==) (T.pack orderedZhNums)
+
+twoDigitNum :: Parser Int
+twoDigitNum = compose <$> option 1 singleNum <* char '十' <*> oneDigit where
+    compose x y = x * 10 + y
+    oneDigit = option 0 singleNum
+
+threeDigitNum :: Parser Int
+threeDigitNum = compose <$> singleNum <* char '百' <*> twoDigit where
+    compose x y = x * 100 + y
+    -- oneDigit = option 0 singleNum
+    twoDigit = try twoDigitNum <|> (char '零' *> singleNum) <|> (pure 0)
+
+threeDigitCapNum :: Parser Int
+threeDigitCapNum = try threeDigitNum <|> try twoDigitNum <|> try singleNum
+
+chapterTitle :: Parser (Int, Text)
+chapterTitle = try normalTitle <|> pure (-1, "sth") where
+    normalTitle = compose <$> (normalChapNum <|> (string "尾声" *> pure 0)) <* spaces <*> (T.pack <$> many (satisfy isPrint))
+    normalChapNum = (char '第' *> threeDigitCapNum) <* char '章'
+    compose 0 b = (0, "尾声 " <> b)
+    compose a b = (a, b)
